862d841f9aa93956cbc0532dd475982a005de6c0
aliyu <1324149139@qq.com>
2022-05-11 17:33:18 +0800

    通过代码跳转路由

--- 14-前端路由/learnvuerouter/src/App.vue
+++ 14-前端路由/learnvuerouter/src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="app">
-    <router-link to="/home" tag="button">home</router-link>
-    <router-link to="/about" tag="button">about</router-link>
-    <router-view/>
+    <h2>我是App组件</h2>
+    <button @click="homeclick">首页</button>
+    <button @click="aboutclick">关于</button>
+<!--    <router-link to="/home" tag="button">home</router-link>-->
+<!--    <router-link to="/about" tag="button">about</router-link>-->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    homeclick() {
+      // this.$router.push('/aaa').catch(error => error)
+      this.$router.replace('/home').catch(error => error)
+    },
+    aboutclick() {
+      this.$router.push('/about').catch(error => error)
+    }
+  }
 }
 </script>
 
 <style>
-.active{
+.active {
   color: red;
 }
 </style>
