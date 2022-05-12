8e5ce25befc321e6f946ef20fca54c28731a962e
aliyu <1324149139@qq.com>
2022-05-12 14:38:58 +0800

    动态路由

---
+++ 14-前端路由/learnvuerouter/src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h2>我是用户</h2>
+  {{userId}}
+  <h2>{{$route.params.id}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  computed:{
+    userId(){
+      return this.$route.params.id;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 14-前端路由/learnvuerouter/src/components/profile.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  用户配置信息
+  {{$route.query.name}}
+  {{$route.query.sex}}
+  {{$route.query.height}}
+</div>
+</template>
+
+<script>
+export default {
+  name: "profile"
+}
+</script>
+
+<style scoped>
+
+</style>
