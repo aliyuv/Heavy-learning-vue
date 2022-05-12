f390f55466c6c861603688983e44bff2f8f0894d
aliyu <1324149139@qq.com>
2022-05-12 18:38:04 +0800

    抽离App.vue文件的代码

--- 15-路由Demo/01-tabbar/src/App.vue
+++ 15-路由Demo/01-tabbar/src/App.vue
@@ -1,38 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <TabBar>
-      <TabBarItem path="/home">
-        <img src="./assets/img/tabbar/home.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/home_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">首页</div>
-      </TabBarItem>
-      <TabBarItem path="/category">
-        <img src="./assets/img/tabbar/category.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/category_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">分类</div>
-      </TabBarItem>
-      <TabBarItem path="/cart">
-        <img src="./assets/img/tabbar/shopcart.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/shopcart_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">购物车</div>
-      </TabBarItem>
-      <TabBarItem path="profile">
-        <img src="./assets/img/tabbar/profile.svg" alt="" slot="item-icon">
-        <img src="./assets/img/tabbar/profile_active.svg" alt="" slot="item-icon-active">
-        <div slot="item-text">我的</div>
-      </TabBarItem>
-    </TabBar>
+    <MainTabBar></MainTabBar>
   </div>
 </template>
 
 <script>
-import TabBar from "./components/tabbar/TabBar";
-import TabBarItem from "./components/tabbar/TabBarItem";
+import MainTabBar from './components/mainTabbar/MainTabBar'
 
 export default {
   name: 'App',
-  components: {TabBar, TabBarItem}
+  components: {MainTabBar}
 }
 </script>
 
